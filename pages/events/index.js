b3f94534079ee15339aa8fb9fca563138afda5e9
Huseyin Soylu <huseyinnsoylu034@gmail.com>
2023-12-29 10:53:03 +0300

    search improvment

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import { Fragment } from "react";
 
 import { getAllEvents } from "../../dummy-data";
@@ -5,11 +6,18 @@ import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 
 function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
 
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
   );
